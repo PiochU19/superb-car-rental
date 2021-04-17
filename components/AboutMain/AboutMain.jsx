d2e5a0ffffd6972413a6444142047caaf8be49c2
PiochU19 <792954018@wp.pl>
2021-04-17 02:17:02 +0200

    About page

---
+++ components/AboutMain/AboutMain.jsx
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react';
+
+import Image from 'next/image';
+
+import styles from './AboutMain.module.scss';
+
+import GitHubSVG from '../../assets/svgs/github.svg';
+
+
+const AboutMain = () => {
+	return (
+		<div className={styles.Container}>
+			<div className={styles.Title}>
+				<h1>Who am I?</h1>
+			</div>
+			<div className={styles.DescMyself}>
+				<h3>Hi there, my name is Dominik and this is my another project written in Django and NextJS <br />Feel free to contact me if you want to login as a employee</h3>
+			</div>
+			<div className={styles.Title}>
+				<h1>About the project</h1>
+			</div>
+			<div className={styles.DescProject}>
+				<h3>
+					This project was written in Django and NextJS, in exactly 19 days <br />
+					During the writing I noticed that a lot of things should be done differently and I'm glad I was able to saw that<br />
+					Honestly it was my first touch with Next, so a lot of time has been taken by reading documentation<br /><br />
+					GitHub Repo below
+				</h3>
+			</div>
+			<div>
+				<GitHubSVG className={styles.SVG} />
+			</div>
+		</div>
+	)
+}
+
+export default AboutMain
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
 import HeadComponent from '../components/HeadComponent/HeadComponent';
 import Navbar from '../components/Navbar/Navbar';
+import AboutMain from '../components/AboutMain/AboutMain';
 import Footer from '../components/Footer/Footer';
 
 
@@ -8,6 +9,7 @@ export default function Home() {
     <>
     	<HeadComponent>Superb Car Rental - About</HeadComponent>
     	<Navbar />
+    	<AboutMain />
     	<Footer />
     </>
   )
